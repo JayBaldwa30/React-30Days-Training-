60bf785f92166c0a021ed9815391c06011ff04a0
JayBaldwa30 <jaybaldwa123@gmail.com>
2023-11-01 00:11:54 +0530

    Added a greeting message component

---
+++ src/components/GreetingMessage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function GreetingMessage() {
+    return (
+        <>
+            <h1>Hello, Welcome to My React App!</h1>
+            <p>I hope you are doing well. Feel free to explore !</p>
+            <p>Thank you for visiting.</p>
+        </>
+
+    )
+}
+
+export default GreetingMessage
