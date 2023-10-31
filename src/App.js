a52e11b18f9da2247f033168b1d22caec3d05953
JayBaldwa30 <jaybaldwa123@gmail.com>
2023-11-01 01:03:59 +0530

    Added about us page component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import AboutUs from './components/AboutUs';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-     
+
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<AboutUs />} />
+      </Routes>
+
     </div>
   );
 }
---
+++ src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AboutUs() {
+    return (
+        <>
+            <h1>AboutUs</h1>
+            <p>This is About Us page</p>
+
+        </>
+    )
+}
+
+export default AboutUs
