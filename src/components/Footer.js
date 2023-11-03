4216f19c310811d5852004cf992ab975253ff174
JayBaldwa30 <jaybaldwa123@gmail.com>
2023-11-03 13:49:52 +0530

    added header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <h1>Footer</h1>
+            <p> &copy; 2023 Jay Baldwa. All rights reserved.</p>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <h1>Header</h1>
+    <p>this will contain the header content </p>
+    </>
+  )
+}
+
+export default Header
