ae8409258cf36001648a64eb3c8b9aaee25da410
JayBaldwa30 <jaybaldwa123@gmail.com>
2023-11-03 13:26:04 +0530

    added a navigation menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='service' element={<Services />} />
+          <Route path='contact' element={<Contact />} />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
